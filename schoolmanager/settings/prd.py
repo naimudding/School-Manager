a6fb3132e846f41e1b8dfab6c570ae5d11cd1393
Naimuddin <naeemgouri.123@gmail.com>
2022-05-15 20:57:48 +0530

    fixed widget-tweaks

--- schoolmanager/settings/prd.py
+++ schoolmanager/settings/prd.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'widget_tweaks',
     'main_website',
     'user',
     'students'
